a2287d7dabb16addad2487fca596240301827216
Luiz Antonio <115804793+MrPalmaBR@users.noreply.github.com>
2024-05-21 22:41:28 -0300

    Adionado sketch.js

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let cor;
+
+let circuloX;
+let circuloY;
+
+let x2;
+let y2;
+
+function setup() {
+  createCanvas(windowWidth - 1, windowHeight - 1); // width x height
+  background("yellow");
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+  circuloX = [0, 0, 0, 0, 0];
+  circuloY = [random(height), random(height), random(height), random(height), random(height)];
+
+  // Chama a função para alterar a cor a cada 1 segundo
+  setInterval(mudarCor, 1000);
+}
+
+function mudarCor() {
+  cor = color(random(0, 255), random(0, 255), random(0, 255));
+}
+
+function draw() {
+  fill(cor);
+  for (let contador in circuloX) {
+    circle(circuloX[contador], circuloY[contador], 50);
+    circuloX[contador] += random(0, 3);
+    circuloY[contador] += random(-3, 3);
+
+    if (circuloX[contador] >= width) {
+      circuloX[contador] = 0;
+      circuloY[contador] = random(height);
+    }
+  }
+}
